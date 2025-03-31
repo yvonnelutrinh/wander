ef750d6a11b474bfd23ef55516f31b39f318c1d4
ylutrinh <yvonnelutrinh@gmail.com>
2025-03-31 19:10:45 -0400

    clean up, remove unused dependencies

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "motion/react";
 import { observer } from "mobx-react-lite";
 import { useContext, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
