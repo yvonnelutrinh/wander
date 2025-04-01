f05fc96348d360ef5f3811b7a938c7676d9c94cb
ylutrinh <yvonnelutrinh@gmail.com>
2025-03-31 21:03:45 -0400

    clean up audio files

--- src/data/sfxData.js
+++ src/data/sfxData.js
@@ -1,17 +1,9 @@
 const soundEffects = {
-    "windChimesMedium": { src: "audio/koshi-wind-chimes-medium.mp3", volume: 0.2 },
-    "windChimesSlow": { src: "audio/koshi-wind-chimes-slow-1.mp3", volume: 0.8 },
-    "windChimesSlow2": { src: "audio/koshi-wind-chimes-slow-2.mp3", volume: 0.9 },
-    "tingshaCymbal": { src: "audio/tingsha-cymbal.mp3", volume: 0.8 },
-    "walking": { src: "audio/walking.mp3", volume: 0.8 },
-    "paisteGong": { src: "audio/brilliant-paiste-32-gong.mp3", volume: 0.8 }
-
+    "windChimesMedium": { src: "audio/koshi-wind-chimes-medium.mp3", volume: 0.2 }
 };
 
 const music = {
     "intro": { src: "audio/intro.mp3", volume: 0.8 },
-    "lo-fi": { src: "audio/lo-fi.mp3", volume: 0.8 },
-    "playful": { src: "audio/playful.mp3", volume: 0.8 },
     "whimsical": { src: "audio/whimsical.mp3", volume: 0.05, loop: true },
     "end": { src: "audio/end.mp3", volume: 0.3, loop: 2 }
 }
